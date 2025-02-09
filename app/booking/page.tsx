e29b1e0f1b6093d5f5dada742097cd5f7eb6e973
Kirztyle <zmret13@gmail.com>
2025-02-09 15:27:05 +0700

    Initial commit

--- app/booking/page.tsx
+++ app/booking/page.tsx
@@ -18,7 +18,7 @@ export default function AdminBookingDashboardCards() {
         }
         const data = await res.json();
         setBookings(data);
-      }
+      },
     }
     fetchBookings();
   }, []);
